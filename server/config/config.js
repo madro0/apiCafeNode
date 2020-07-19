53a1bc95e72b10dbf829aa0e8601ef7325d2ee13
Madro0 <daanii2013@gmail.com>
2020-07-18 20:44:34 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://madro:K8mwWFepr8Gmc73s@cluster0.yugug.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
